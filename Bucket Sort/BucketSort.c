250f30e735d7b66041c725904d4fc53bfe6c4096
TechbyWebCoder <techbywedcoder@gmail.com>
2026-05-11 18:23:24 +0530

    Implement bucket sort algorithm in BucketSort.c

---
+++ Bucket Sort/BucketSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void bucketSort(float arr[], int n) {
+    int i, j;
+    float temp;
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    float arr[] = {0.42, 0.32, 0.23, 0.52, 0.25, 0.47};
+    int n = 6;
+
+    bucketSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%.2f ", arr[i]);
+
+    return 0;
+}
