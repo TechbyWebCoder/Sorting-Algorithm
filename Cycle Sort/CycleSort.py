e6f5205fb3fb73f57d98068510c852cc90f86d23
TechbyWebCoder <techbywedcoder@gmail.com>
2026-05-14 18:03:29 +0530

    Add Cycle Sort implementation in CycleSort.py
    Implement Cycle Sort algorithm to sort an array in place.

---
+++ Cycle Sort/CycleSort.py
@@ -0,0 +1,37 @@
+def cycle_sort(arr):
+    n = len(arr)
+
+    for cycle_start in range(0, n - 1):
+
+        item = arr[cycle_start]
+        pos = cycle_start
+
+        for i in range(cycle_start + 1, n):
+            if arr[i] < item:
+                pos += 1
+
+        if pos == cycle_start:
+            continue
+
+        while item == arr[pos]:
+            pos += 1
+
+        arr[pos], item = item, arr[pos]
+
+        while pos != cycle_start:
+
+            pos = cycle_start
+
+            for i in range(cycle_start + 1, n):
+                if arr[i] < item:
+                    pos += 1
+
+            while item == arr[pos]:
+                pos += 1
+
+            arr[pos], item = item, arr[pos]
+
+arr = [20, 40, 50, 10, 30]
+cycle_sort(arr)
+
+print(arr)
