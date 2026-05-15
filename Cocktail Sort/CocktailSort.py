d9cba2a4047e94f437f31ef351aa5d19bd286fac
TechbyWebCoder <techbywedcoder@gmail.com>
2026-05-15 18:38:01 +0530

    Add cocktail sort implementation
    Implement cocktail sort algorithm to sort an array.

---
+++ Cocktail Sort/CocktailSort.py
@@ -0,0 +1,32 @@
+def cocktail_sort(arr):
+    n = len(arr)
+    swapped = True
+    start = 0
+    end = n - 1
+
+    while swapped:
+
+        swapped = False
+
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        end -= 1
+
+        for i in range(end - 1, start - 1, -1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        start += 1
+
+arr = [5, 1, 4, 2, 8, 0, 2]
+cocktail_sort(arr)
+
+print(arr)
