c361d5d5359498c790775f3c8d44f4914b71f3c8
TechbyWebCoder <techbywedcoder@gmail.com>
2026-05-21 17:41:37 +0530

    Add Pancake Sort implementation in PancakeSort.cpp
    Implement Pancake Sort algorithm in C++.

---
+++ Pancake Sort/PancakeSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void flip(int arr[], int i) {
+    int start = 0;
+
+    while(start < i) {
+        swap(arr[start], arr[i]);
+        start++;
+        i--;
+    }
+}
+
+int findMax(int arr[], int n) {
+    int maxIndex = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > arr[maxIndex])
+            maxIndex = i;
+    }
+
+    return maxIndex;
+}
+
+void pancakeSort(int arr[], int n) {
+
+    for(int currSize = n; currSize > 1; currSize--) {
+
+        int maxIndex = findMax(arr, currSize);
+
+        if(maxIndex != currSize - 1) {
+
+            flip(arr, maxIndex);
+
+            flip(arr, currSize - 1);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {23, 10, 20, 11, 12, 6, 7};
+    int n = 7;
+
+    pancakeSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
