24e0b0d359862b30a1ac49bfc41a98178612c174
TechbyWebCoder <techbywedcoder@gmail.com>
2026-05-27 20:48:04 +0530

    Implement Stooge Sort algorithm in StoogeSort.cpp

---
+++ Stooge  Sort/StoogeSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void stoogeSort(int arr[], int low, int high) {
+
+    if(arr[low] > arr[high])
+        swap(arr[low], arr[high]);
+
+    if(high - low + 1 > 2) {
+
+        int t = (high - low + 1) / 3;
+
+        stoogeSort(arr, low, high - t);
+        stoogeSort(arr, low + t, high);
+        stoogeSort(arr, low, high - t);
+    }
+}
+
+int main() {
+
+    int arr[] = {2, 4, 5, 3, 1};
+    int n = 5;
+
+    stoogeSort(arr, 0, n - 1);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
